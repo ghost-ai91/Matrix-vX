3cd275e64293f2469ac1988dde3add0b2c4a8fbd
DONUT Matrix Deployment <deployment@donut-matrix.com>
2025-06-20 18:32:45 -0300

    Updated version of DONUT Referral Matrix System

--- programs/matrix-system/src/lib.rs
+++ programs/matrix-system/src/lib.rs
@@ -8,8 +8,7 @@ use solana_program::program::invoke;
 #[cfg(not(feature = "no-entrypoint"))]
 // use {solana_security_txt::security_txt};
 
-
-declare_id!("GBpSgsG8KguJxaEJBm26TuCh2psatt8iMwuvy8h1rMQt");
+declare_id!("BLCwTa5k6YB5oDqLDFnSYiNabhXos1vAKrUTRMufZDXK");
 
 // #[cfg(not(feature = "no-entrypoint"))]
 // security_txt! {
